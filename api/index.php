e1090de9478594d5d621faf2a137137b9ddb9668
natsux <husee123@126.com>
2018-06-02 19:16:02 +0800

    add php api

--- api/index.php
+++ api/index.php
@@ -1,15 +1,1 @@
-<?php
-require "response.class.php";    //引入返回信息类
-
-//准备返回数据
-$code = 1;
-$message = "success";
-$data = array(
-    "name" => "xxx",
-);
-
-//实例化response类
-$response = new Response;
-
-//返回数据
-echo $response -> json($code,$message,$data);
+1
